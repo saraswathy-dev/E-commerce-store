516dd333ebb83ae57b90204cd8c2742c120ff3c1
saraswathy <saraswathyrajendran.o@gmail.com>
2025-07-11 19:26:53 +0200

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import './App.css'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/signup";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-<Routes>
-      <Route path="/" element={<Home />} />
-</Routes>
-    </>
-  )
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+      {/* background gradient */}
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-radial-[at_50%_75%] from-sky-200 via-blue-400 to-indigo-400 to-90%"></div>
+        </div>
+      </div>
+      {/* main content */}
+      <div className="relative z-10 p-20">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const user = false; // Replace with actual user state management
+  const isAdmin = false; // Replace with actual admin state management
+  return (
+    <>
+      <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800 ">
+        <div className="container mx-auto px-2 py-4 flex justify-between items-center   ">
+          <Link
+            to="/"
+            className="text-2xl font-bold text-white-800 items-center space-x-2 flex hover:text-blue-300 transition-colors duration-300"
+          >
+            E-Commerce
+          </Link>
+          <nav className="flex items-center  space-x-4">
+            <Link
+              to="/"
+              className="text-white hover:text-blue-800 transition-colors duration-300"
+            >
+              Home
+            </Link>
+           
+            {user && (
+              <Link
+                to="/cart"
+                className="relative text-white hover:text-blue-900 transition-colors duration-300 flex items-center"
+              >
+                <div className="relative">
+                  <ShoppingCart className="inline-block" size={24} />
+                  <span className="absolute -top-2 -right-2 bg-blue-600 text-white text-xs px-1.5 py-0.5 rounded-full">
+                    3
+                  </span>
+                </div>
+                <span className="ml-2">Cart</span>
+              </Link>
+            )}
+            {isAdmin && (
+              <Link
+                to="/admin"
+                className="bg-blue-800 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors duration-300 flex items-center"
+              >
+                <Lock className="inline-block mr-1" size={18}></Lock>
+                <span className="hidden sm:inline">Dashboard</span>
+              </Link>
+            )}
+            {user ? (
+              <button className="bg-gray-500 text-white px-2 py-2 rounded-md hover:bg-gray-700 transition-colors duration-300 flex items-center">
+                <LogOut className="inline-block" size={24} />
+                <span className="ml-2">Logout</span>
+              </button>):(
+                <>
+                <Link
+                  to="/signup"
+                  className="bg-blue-800 text-white px-4 py-2 rounded-md hover:bg-gray-700 transition-colors duration-300 flex items-center"
+                >
+                    <UserPlus className="inline-block" size={24} />
+                    <span className="ml-2">Signup</span>
+                </Link>
+                <Link
+                  to="/login"
+                  className="bg-gray-800 text-white px-4 py-2 rounded-md hover:bg-gray-700 transition-colors duration-300 flex items-center ml-2"
+                >
+                    <LogIn className="inline-block" size={24} />
+                    <span className="ml-2">Login</span>
+                </Link>
+                </>
+               
+                
+               
+              )}
+         
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
